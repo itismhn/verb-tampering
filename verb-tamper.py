44f499285738674562078cc5581bf224e923a4bb
itismhn <itismhn@yahoo.com>
2023-03-11 01:10:40 +0330

    base program + handl inputs

--- verb-tamper.py
+++ verb-tamper.py
@@ -1,6 +1,9 @@
-import sys
+import sys,requests
+arg=sys.argv
 
-if len(sys.argv) > 2:
-    print('not')
+    
+if len(sys.argv)==1:
+    print('[~]Enter in valid format :\n\t>> python verb-tamper.py <URL>')
 else:
-    print('yep')
+    url=arg[1]
+    print(url)
